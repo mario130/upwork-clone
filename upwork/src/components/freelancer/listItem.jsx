bd915078ca39a253dbc2a863204cf11ce7651def
mario130 <mario.yonan.work@gmail.com>
2021-04-06 22:51:02 +0200

    finished freelancer homepage navbar

--- upwork/src/components/freelancer/listItem.jsx
+++ upwork/src/components/freelancer/listItem.jsx
@@ -6,29 +6,39 @@ const ListItem = (props) => {
   return (
     <div>
       <div className="flex justify-between items-center border-b border-navBorder pt-4 cursor-pointer" onClick={()=>setIsOpen(!isOpen)}>
-        {/* user icon */}
         <div className="flex container mx-auto items-center pb-4">
+          {props.list.rightIcon}
           <h4 className="text-sm">{props.list.title}</h4>
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 md:hidden mb-4"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M19 9l-7 7-7-7"
-          />
-        </svg>
+
+        {/* has dropdown? */}
+        {props.list.list
+        ? <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 mb-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+      : null}
+
+      {/* has another icon? */}
+      {props.list.icon
+      ? <div className="mb-4">{props.list.icon}</div>
+      : null}
+      
       </div>
       {/* list */}
       {isOpen ? (
         <ul>
-          {props.list.list.map((item) => (
+          {props.list.list?.map((item) => (
             <li className="pt-4 ml-4 text-sm">{item}</li>
           ))}
         </ul>
--- upwork/src/components/freelancer/navbar.jsx
+++ upwork/src/components/freelancer/navbar.jsx
@@ -31,11 +31,115 @@ const Nav = () => {
         "Certificate of Earnings",
       ],
     },
+    {
+      title: "Messages",
+    },
+    {
+      title: "Help",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      ),
+    },
+    {
+      title: "Notifications",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+          />
+        </svg>
+      ),
+    },
+    {
+      title: "Direct Contracts",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+          />
+        </svg>
+      ),
+    },
+    {
+      title: "Settings",
+      rightIcon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 mr-3"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+      ),
+    },
+    {
+      title: "Log out",
+      rightIcon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 mr-3"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+          />
+        </svg>
+      ),
+    },
   ]);
 
   return (
     <div className="border-b border-gray-200 mb-3 bg-complementary z-50 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto max-w-5xl">
         <div className="nav-wrapper px-3 mr-1 flex items-center justify-between h-14">
           {/* hamburger */}
           <button
@@ -85,7 +189,7 @@ const Nav = () => {
           {/* search icon */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-6 w-6 lg:hidden"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -97,6 +201,57 @@ const Nav = () => {
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
+
+          {/* desktop search */}
+          <div className="flex-1 mx-3 p-3 relative mt-2 hidden lg:block">
+            <div className="container mx-auto pb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-5 absolute top-6 left-5 text-white text-sm"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                type="text"
+                className="w-full rounded-full py-2 px-2 pl-9 focus:outline-none bg-twilight placeholder-white"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+
+          {/* desktop links */}
+          <div className="hidden lg:block">
+            <ul className="flex space-x-5">
+              <li>Find Work</li>
+              <li>My Jobs</li>
+              <li>Reports</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+
+          {/* desktop icons */}
+          <ul className="space-x-4 hidden lg:flex">
+            {navLists.map((list) => (
+              <li>{list.icon ? list.icon : null}</li>
+            ))}
+          </ul>
+
+          <div className="hidden lg:block">
+            <img
+              src="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
+              width="40"
+              className="mr-2"
+              alt=""
+            />
+          </div>
         </div>
 
         {/* nav mobile open */}
@@ -137,22 +292,8 @@ const Nav = () => {
                     className="mr-2"
                     alt=""
                   />
-                  <h4 className="text-sm">Mario Magdy</h4>
+                  <h4 className="text-sm">John Doe</h4>
                 </div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 md:hidden mb-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
               </div>
             </div>
 
