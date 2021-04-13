11a93beea936021b97f950a80cd0fea5524ca70e
Islam Mohamed <islammohamed2552@gmail.com>
2021-04-13 09:08:17 +0200

    User Authentication done

---
+++ backend/controllers/userController.js
@@ -0,0 +1,113 @@
+const User = require('../model/user');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+module.exports.register = (req,resp,next) => {
+    let user=new User();
+    user.firstName=req.body.firstName;
+    user.email=req.body.email;
+    user.lastName=req.body.lastName;
+    user.password=req.body.password;
+    user.userName=req.body.userName;
+    user.country=req.body.country;
+    user.userType=req.body.userType;
+    user.save((err,doc) =>{
+        if(!err)
+            resp.send(doc);
+        else{
+            if(err.code == 11000)
+                resp.status(422).send('E-mail already exists');
+            else
+                return next(err);
+        }
+    })
+
+}
+
+module.exports.authenticate = (req,resp,next) => {
+    passport.authenticate('local',(err , user, info) => {
+        // check if there is an error from passport middleware
+        if(err)
+          return resp.status(400).json(err);
+        // registered user
+        else if(user)
+          return resp.status(200).json({'token': user.generateJwt() });
+        // Unknown user or wrong password
+        else
+          return resp.status(404).json(info);
+      })(req,resp)
+}
+
+module.exports.authMail = (req,resp,next) => {
+    passport.authenticate('local',(err , user, info) => {
+        // check if there is an error from passport middleware
+        if(err)
+          return resp.status(400).json(err);
+        // registered user
+        else if(user)
+          return resp.status(200).send(user);
+        // Unknown user or wrong password
+        else
+          return resp.status(404).json(info);
+      })(req,resp)
+}
+
+module.exports.getAll = (req,resp,next) => {
+    User.find({},(err,data) => {
+        if(!err){
+            resp.status(200).send(data)
+         }else return next(err)
+    })
+}
+
+module.exports.getById = (req,resp,next) => {
+    User.findOne({_id:req.params.id},(err,data) => {
+        if(!err)
+            resp.status(200).send(data)
+        else
+            return next(err)
+    })
+}
+
+module.exports.getByEmail = (req,resp,next) => {
+    User.findOne({email:req.params.email},(err,data) => {
+        if(!err)
+            resp.status(200).send('Email exists')
+        else
+            return next(err)
+    })
+}
+
+/* module.exports.update = (req,resp,next) => {
+    let user=req.body
+    User.updateOne({_id:req.params.id},user,(err,data) => {
+        if(!err){
+            if(user.password){
+                req.params.password = bcrypt.hashSync(req.params.password,10)
+            }
+            resp.status(200).send(data)
+        }else return next(err)
+    })
+} */
+
+module.exports.update = (req,resp,next) => {
+    let user=req.body
+    User.updateOne({_id: req.params.id},user).then((data, err) => {
+        if (!err) {
+          if(data === null){
+            return resp.status(200).send('Not found')
+          }
+          return resp.status(200).send(data)
+        } else {
+          resp.status(400).send(err)
+        }
+    })
+}
+
+module.exports.delete = (req,resp,next) => {
+    User.deleteOne({_id:req.params.id},(err,data) => {
+        if(!err){
+            resp.status(200).send(data)
+        }else return next(err)
+    })
+}
---
+++ backend/model/user.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
+const userSchema = mongoose.Schema({
+    firstName:{
+        type: String,
+        required: "First name field can\'t be empty"
+    },
+    lastName:{
+        type: String,
+        required: "Last name field can\'t be empty"
+    },
+    email:{
+        type:String,
+        required: "Email field can\'t be empty",
+        unique:true
+    },
+    password:{
+        type:String,
+        required: "password field can\'t be empty",
+        minlength: [6,"password can\'t be less than 6 characters"]
+    },
+    saltSecret:{
+        type:String
+    },
+    userName:{
+        type:String,
+        required:   "User name field can\'t be empty",
+        unique:true
+    },
+    country:{
+        type:String,
+        required: "country field can\'t be empty"
+    },
+    userType:{
+        type:String,
+        required: "userType field can\'t be empty"
+    }
+})
+
+//Custom Validation
+userSchema.path('email').validate((val) => {
+    emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+    return emailRegex.test(val)
+},'Invalid E-mail address')
+
+// Pre Event
+userSchema.pre('save',function(next){
+    bcrypt.genSalt(10,(err,salt) => {
+        bcrypt.hash(this.password,salt,(err,hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+});
+
+// Methods
+userSchema.methods.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+userSchema.methods.generateJwt = function () {
+    return jwt.sign({ _id: this._id},
+        process.env.JWT_SECRET,{
+            expiresIn: process.env.JWT_EXP
+        });
+}
+
+
+module.exports = mongoose.model("User", userSchema);
