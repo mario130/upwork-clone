d72273bc0b1ab8dfe7333c29876cbd072a70e81e
Islam Mohamed <islammohamed2552@gmail.com>
2021-04-21 14:22:58 +0200

    upload file for proposal

--- backend/routes/proposalRoutes.js
+++ backend/routes/proposalRoutes.js
@@ -3,9 +3,29 @@ const router = express.Router();
 const proposalControl = require('../controllers/proposalController');
 const jwtHelper = require('../middleware/jwtHelper');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null, 'assets/uploads/files');
+    },
+    filename: function(req,file,cb) {
+        cb(null,Date.now() + file.originalname);
+    }
+});
+const fileFilter = (req,file,cb) => {
+    if(file.mimetype === 'application/msword' || file.mimetype === 'application/vnd.openxmlformats' || file.mimetype === 'application/pdf'){
+        cb(null,true);
+    }else{
+        cb(new Error(),false);
+    }
+}
+const upload = multer({storage:storage, limits:{
+    fileSize: 1024 * 1024 * 10
+},fileFilter:fileFilter});
+
 
 router.get('/getFreelancerProposals',jwtHelper.verifyJwtToken,proposalControl.getFreelancerProposoal);
 router.get('/getJobProposals/:jobId',jwtHelper.verifyJwtToken,proposalControl.getJobProposals);
-router.post('/add/:jobId',jwtHelper.verifyJwtToken, proposalControl.addProposal);
+router.post('/add/:jobId',upload.single('imgPath'),jwtHelper.verifyJwtToken, proposalControl.addProposal);
 
 module.exports = router;
