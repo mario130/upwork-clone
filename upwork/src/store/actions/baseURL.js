1f7aed82bd8641ec7311b377d952ce8d65632f45
mario130 <mario.yonan.work@gmail.com>
2021-04-22 13:00:51 +0200

    switched localhost to server

--- upwork/src/store/actions/baseURL.js
+++ upwork/src/store/actions/baseURL.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:4001";
-// const baseURL = "https://upwork-4.herokuapp.com";
+// const baseURL = "http://localhost:4001";
+const baseURL = "https://upwork-4.herokuapp.com";
 
 export default baseURL;
