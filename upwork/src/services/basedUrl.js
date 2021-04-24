5650d4e1b2ae5d1bc454905692a33134fdc4af47
mario130 <mario.yonan.work@gmail.com>
2021-04-24 20:32:55 +0200

    change backend url dynamically

--- upwork/src/services/basedUrl.js
+++ upwork/src/services/basedUrl.js
@@ -1,5 +1,14 @@
-export const localBackend ="http://localhost:4001"
+// export const localBackend ="http://localhost:4001"
 export const localFrontend ="http://localhost:3000"
 
 export const serverBackend ="https://upwork-4.herokuapp.com"
 export const serverFrontend ="https://upwork-4.web.app/"
+
+export let localBackend;
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env.NODE_ENV);
+  localBackend ="http://localhost:4001"
+} else {
+  localBackend ="https://upwork-4.herokuapp.com"
+}
